dfb2fa52749ff00fa13181f4e5d11433dd06c6cb
Jan Hofmeier <jan.hofmeier@de.ibm.com>
2022-02-08 09:29:15 +0100

    reuse buffers

--- src/main/java/com/ibm/lozperf/mb/modeladapter/DLCSplitModelBatchingAdapter.java
+++ src/main/java/com/ibm/lozperf/mb/modeladapter/DLCSplitModelBatchingAdapter.java
@@ -1,5 +1,6 @@
 package com.ibm.lozperf.mb.modeladapter;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,26 +17,40 @@ import com.ibm.onnxmlir.OMTensorList;
 
 public class DLCSplitModelBatchingAdapter extends TFJavaBatchingAdapter {
 
+	
+	private class BufferStruct{
+		
+		final long[] shape;
+		final FloatDataBuffer preprocessedDB;
+		final float[] preprocessedArr;
+		
+		public BufferStruct(int bs, int size) {
+			final int nTS = numberTimesteps();
+			preprocessedDB = DataBuffers.ofFloats(size);
+			preprocessedArr = new float[size];
+			shape = new long[]{ nTS, bs, preprocessedArr.length / (nTS * bs) };
+
+		}
+		
+	}
+	
+	ThreadLocal<HashMap<Integer,BufferStruct>> threadBuffers = ThreadLocal.withInitial(() -> new HashMap<Integer,BufferStruct>());
+	
 	@Override
 	protected void batchPredict(List<Job<Inputs>> batch) {
 		final int nTS = numberTimesteps();
 		Map<String, Tensor> map = tensorfyInputs(batch);
 		map = smb.call(map);
 		TFloat32 preprocessedTF32 = (TFloat32) map.get("tf.compat.v1.transpose");
-		map = null;
-		FloatDataBuffer preprocessedDB = DataBuffers.ofFloats(preprocessedTF32.size());
-		preprocessedTF32.read(preprocessedDB);
-		preprocessedTF32 = null;
-		float[] preprocessedArr = new float[(int) preprocessedDB.size()];
-		preprocessedDB.read(preprocessedArr);
-		preprocessedDB = null;
-		long[] shape = { nTS, batch.size(), preprocessedArr.length / (nTS * batch.size()) };
-		OMTensor tctensor = new OMTensor(preprocessedArr, shape);
-		preprocessedArr = null;
+		
+		BufferStruct buffers = threadBuffers.get().computeIfAbsent(batch.size(), bs -> new BufferStruct(bs, (int) preprocessedTF32.size()));
+		
+		preprocessedTF32.read(buffers.preprocessedDB);
+		buffers.preprocessedDB.read(buffers.preprocessedArr);
+		OMTensor tctensor = new OMTensor(buffers.preprocessedArr, buffers.shape);
 		OMTensorList tensorList = new OMTensorList(new OMTensor[] { tctensor });
 		tensorList = OMModel.mainGraph(tensorList);
 		float prediction[] = tensorList.getOmtByIndex(0).getFloatData();
-		tensorList = null;
 		for (int i = 0; i < batch.size(); i++) {
 			boolean fraud = prediction[(nTS - 1) * batch.size() + i] > 0.5;
 			batch.get(i).setResult(fraud);
--- src/main/java/com/ibm/lozperf/mb/modeladapter/TFJavaBatchingAdapter.java
+++ src/main/java/com/ibm/lozperf/mb/modeladapter/TFJavaBatchingAdapter.java
@@ -1,8 +1,10 @@
 package com.ibm.lozperf.mb.modeladapter;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.tensorflow.Tensor;
 import org.tensorflow.ndarray.Shape;
@@ -22,46 +24,67 @@ public class TFJavaBatchingAdapter extends TFJavaAdapter {
 	
 	private BatchCollector<Inputs> batchCollector = new BatchCollector<>((batch)-> batchPredict(batch));
 
+	
+	private class BufferStruct{
+		
+		final Shape shape;
+		final TFloat32 amount;
+		final DataBuffer<String> useChip;
+		final DataBuffer<String> merchantName;
+		final DataBuffer<String> merchantCity;
+		final DataBuffer<String> merchantState;
+		final DataBuffer<String> zip;
+		final DataBuffer<String> mcc;
+		final DataBuffer<String> errors;
+		final TInt64 date;
+		
+		public BufferStruct(int batchsize) {
+			int nTS = numberTimesteps();
+			shape = Shape.of(batchsize, nTS);
+			amount = TFloat32.tensorOf(shape);
+			useChip = DataBuffers.ofObjects(String.class, shape.size());
+			merchantName = DataBuffers.ofObjects(String.class, shape.size());
+			merchantCity = DataBuffers.ofObjects(String.class, shape.size());
+			merchantState = DataBuffers.ofObjects(String.class, shape.size());
+			zip = DataBuffers.ofObjects(String.class, shape.size());
+			mcc = DataBuffers.ofObjects(String.class, shape.size());
+			errors = DataBuffers.ofObjects(String.class, shape.size());
+			date = TInt64.tensorOf(shape);
+		}
+		
+	}
+	
+	ThreadLocal<HashMap<Integer,BufferStruct>> threadBuffers = ThreadLocal.withInitial(() -> new HashMap<Integer,BufferStruct>());
+	
 	protected Map<String, Tensor> tensorfyInputs(List<Job<Inputs>> batch){
-		int nTS = numberTimesteps();
-		Shape shape = Shape.of(batch.size(), nTS);
-		TFloat32 amount = TFloat32.tensorOf(shape);
-		DataBuffer<String> useChip = DataBuffers.ofObjects(String.class, shape.size());
-		DataBuffer<String> merchantName = DataBuffers.ofObjects(String.class, shape.size());
-		DataBuffer<String> merchantCity = DataBuffers.ofObjects(String.class, shape.size());
-		DataBuffer<String> merchantState = DataBuffers.ofObjects(String.class, shape.size());
-		DataBuffer<String> zip = DataBuffers.ofObjects(String.class, shape.size());
-		DataBuffer<String> mcc = DataBuffers.ofObjects(String.class, shape.size());
-		DataBuffer<String> errors = DataBuffers.ofObjects(String.class, shape.size());
-		// DataBuffer<Long> date = DataBuffers.ofLongs(shape.size());
-		TInt64 date = TInt64.tensorOf(shape);
-
+		BufferStruct buffers = threadBuffers.get().computeIfAbsent(batch.size(), (bs)->new BufferStruct(bs));
+		int nTS = numberTimesteps();	
 		for (int i = 0; i < batch.size(); i++) {
 			Inputs inputs = batch.get(i).getInput();
 			for (int j = 0; j < nTS; j++) {
-				amount.setFloat(inputs.Amount[j].floatValue(), i, j);
-				date.setLong(inputs.YearMonthDayTime[j], i, j);
+				buffers.amount.setFloat(inputs.Amount[j].floatValue(), i, j);
+				buffers.date.setLong(inputs.YearMonthDayTime[j], i, j);
 
-				useChip.setObject(inputs.UseChip[j], i * nTS + j);
-				merchantName.setObject(inputs.MerchantName[j], i * nTS + j);
-				merchantCity.setObject(inputs.MerchantCity[j], i * nTS + j);
-				merchantState.setObject(inputs.MerchantState[j], i * nTS + j);
-				zip.setObject(inputs.Zip[j], i * nTS + j);
-				mcc.setObject(inputs.MCC[j], i * nTS + j);
-				errors.setObject(inputs.Errors[j], i * nTS + j);
+				buffers.useChip.setObject(inputs.UseChip[j], i * nTS + j);
+				buffers.merchantName.setObject(inputs.MerchantName[j], i * nTS + j);
+				buffers.merchantCity.setObject(inputs.MerchantCity[j], i * nTS + j);
+				buffers.merchantState.setObject(inputs.MerchantState[j], i * nTS + j);
+				buffers.zip.setObject(inputs.Zip[j], i * nTS + j);
+				buffers.mcc.setObject(inputs.MCC[j], i * nTS + j);
+				buffers.errors.setObject(inputs.Errors[j], i * nTS + j);
 			}
 
 		}
-		Map<String, Tensor> inputMap = new HashMap<>();
-		inputMap.put("Amount", amount);
-		inputMap.put("UseChip", TString.tensorOf(shape, useChip));
-		inputMap.put("MerchantName", TString.tensorOf(shape, merchantName));
-		inputMap.put("MerchantCity", TString.tensorOf(shape, merchantCity));
-		inputMap.put("MerchantState", TString.tensorOf(shape, merchantState));
-		inputMap.put("Zip", TString.tensorOf(shape, zip));
-		inputMap.put("MCC", TString.tensorOf(shape, mcc));
-		inputMap.put("Errors", TString.tensorOf(shape, errors));
-		inputMap.put("YearMonthDayTime", date);
+		Map<String, Tensor> inputMap = new HashMap<>(20);
+		inputMap.put("Amount", buffers.amount);
+		inputMap.put("UseChip", TString.tensorOf(buffers.shape, buffers.useChip));
+		inputMap.put("MerchantName", TString.tensorOf(buffers.shape, buffers.merchantName));
+		inputMap.put("MerchantCity", TString.tensorOf(buffers.shape, buffers.merchantCity));
+		inputMap.put("MerchantState", TString.tensorOf(buffers.shape, buffers.merchantState));
+		inputMap.put("Zip", TString.tensorOf(buffers.shape, buffers.zip));
+		inputMap.put("MCC", TString.tensorOf(buffers.shape, buffers.mcc));
+		inputMap.put("Errors", TString.tensorOf(buffers.shape, buffers.errors));
+		inputMap.put("YearMonthDayTime", buffers.date);
 		
 		return inputMap;
 	}
