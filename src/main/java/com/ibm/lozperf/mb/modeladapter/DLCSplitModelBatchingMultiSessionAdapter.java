beffc7ea83a9d83d1ea946b32df6817d3e88873c
Jan Hofmeier <jan.hofmeier@de.ibm.com>
2022-03-02 18:07:43 +0100

    MultiSessionBatchingAdapter

--- src/main/java/com/ibm/lozperf/mb/modeladapter/DLCSplitModelBatchingMultiSessionAdapter.java
+++ src/main/java/com/ibm/lozperf/mb/modeladapter/DLCSplitModelBatchingMultiSessionAdapter.java
@@ -1,32 +1,15 @@
 package com.ibm.lozperf.mb.modeladapter;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.tensorflow.SavedModelBundle;
-import org.tensorflow.SavedModelBundle.Loader;
 import org.tensorflow.types.TFloat32;
 import org.tensorflow.Tensor;
 
 import com.ibm.lozperf.mb.Inputs;
 import com.ibm.lozperf.mb.batching.Job;
 
-public class DLCSplitModelBatchingMultiSessionAdapter extends AbstractBatchingAdapter {
-	
-	public final static String TF_MODEL = System.getenv("TF_MODEL");
-	Loader loader = SavedModelBundle.loader(TF_MODEL);
-	
-	private Collection<SavedModelBundle> smbs = new ArrayList<>();
-	ThreadLocal<SavedModelBundle> threadSmb = new ThreadLocal<SavedModelBundle>() {
-		@Override
-		protected SavedModelBundle initialValue() {		
-			SavedModelBundle smb = loader.load();
-			smbs.add(smb);
-			return smb;
-		}
-	};
+public class DLCSplitModelBatchingMultiSessionAdapter extends TFJavaBatchingMultiSessionIgnoreOutputAdapter {
 	
 	@Override
 	protected void batchPredict(List<Job<Inputs>> batch) {
@@ -40,11 +23,5 @@ public class DLCSplitModelBatchingMultiSessionAdapter extends AbstractBatchingAd
 			boolean fraud = prediction[(nTS - 1) * batch.size() + i] > 0.5;
 			batch.get(i).setResult(fraud);
 		}
-	}
-	
-	@Override
-	public void close() throws Exception {
-		smbs.forEach(SavedModelBundle::close);
-		super.close();
 	}
 }
